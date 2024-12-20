47afdf94613758ff01506638551f7aa380826594
andreadamgaard <124664759+andreadamgaard@users.noreply.github.com>
2024-12-20 01:21:10 +0100

    f

---
+++ src/lib/api.js
@@ -0,0 +1,19 @@
+const rootUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const key = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+
+export const fetchData = async (table) => {
+  const url = `${rootUrl}/${table}`;
+  const response = await fetch(url, {
+    headers: {
+      "Content-Type": "application/json",
+      apikey: key,
+    },
+  });
+  return await response.json();
+};
+
+export const fetchProductData = async () => fetchData("product-data");
+export const fetchProductInfo = () => fetchData("product-info");
+
+export const fetchBoxes = () => fetchData("boxes");
+export const fetchSub = () => fetchData("subscription");
